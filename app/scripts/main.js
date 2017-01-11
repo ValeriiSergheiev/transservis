521d940d3444f7aa3d7e0b8274bec2aabd2c8395
ValeriiSergheiev <Val.Serheiev@gmail.com>
2017-01-11 12:31:23 +0200

    contact page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,207 @@
 // Страница Проекты - Карта
 var map;
+var contact_map;
+var map_styles = [
+{
+	'featureType': 'administrative',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#0080ff'
+	},
+	{
+		'saturation': 29
+	},
+	{
+		'lightness': -52
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'administrative',
+	'elementType': 'labels.text',
+	'stylers': [
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'administrative',
+	'elementType': 'labels.text.stroke',
+	'stylers': [
+	{
+		'visibility': 'off'
+	}
+	]
+},
+{
+	'featureType': 'administrative.locality',
+	'elementType': 'geometry',
+	'stylers': [
+	{
+		'hue': '#ffffff'
+	},
+	{
+		'lightness': 100
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'landscape',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#ffffff'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': 100
+	},
+	{
+		'visibility': 'simplified'
+	}
+	]
+},
+{
+	'featureType': 'landscape.man_made',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#ffffff'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': 100
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'landscape.natural',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#ffffff'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': 100
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'poi',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#999999'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': -23
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'poi.park',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#ffffff'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': 100
+	},
+	{
+		'visibility': 'off'
+	}
+	]
+},
+{
+	'featureType': 'poi.school',
+	'elementType': 'labels',
+	'stylers': [
+	{
+		'hue': '#999999'
+	},
+	{
+		'saturation': -100
+	},
+	{
+		'lightness': -28
+	},
+	{
+		'visibility': 'on'
+	}
+	]
+},
+{
+	'featureType': 'road',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'hue': '#00559B'
+	},
+	{
+		'saturation': 100
+	},
+	{
+		'lightness': -53
+	},
+	{
+		'visibility': 'simplified'
+	}
+	]
+},
+{
+	'featureType': 'water',
+	'elementType': 'all',
+	'stylers': [
+	{
+		'saturation': 100
+	},
+	{
+		'lightness': -60
+	},
+	{
+		'visibility': 'simplified'
+	},
+	{
+		'color': '#c9deff'
+	}
+	]
+}
+];
+
 function initMap() {
 	var myLatLng = {lat: 53.311315, lng: 50.528891};
-	map = new google.maps.Map(document.getElementById('map'), {
+	var myOptions = {
 		center: myLatLng,
 		zoom: 9,
 		scrollwheel: false,
@@ -11,232 +210,11 @@ function initMap() {
 		streetViewControl: false,
 		rotateControl: false,
 		fullscreenControl: false,
-		styles: [
-		{
-			'featureType': 'administrative',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#0080ff'
-			},
-			{
-				'saturation': 29
-			},
-			{
-				'lightness': -52
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'administrative',
-			'elementType': 'labels.text',
-			'stylers': [
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'administrative',
-			'elementType': 'labels.text.stroke',
-			'stylers': [
-			{
-				'visibility': 'off'
-			}
-			]
-		},
-		{
-			'featureType': 'administrative.locality',
-			'elementType': 'geometry',
-			'stylers': [
-			{
-				'hue': '#ffffff'
-			},
-			{
-				'lightness': 100
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'landscape',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#ffffff'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': 100
-			},
-			{
-				'visibility': 'simplified'
-			}
-			]
-		},
-		{
-			'featureType': 'landscape.man_made',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#ffffff'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': 100
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'landscape.natural',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#ffffff'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': 100
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'poi',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#999999'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': -23
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'poi.park',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#ffffff'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': 100
-			},
-			{
-				'visibility': 'off'
-			}
-			]
-		},
-		{
-			'featureType': 'poi.school',
-			'elementType': 'labels',
-			'stylers': [
-			{
-				'hue': '#999999'
-			},
-			{
-				'saturation': -100
-			},
-			{
-				'lightness': -28
-			},
-			{
-				'visibility': 'on'
-			}
-			]
-		},
-		{
-			'featureType': 'road',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'hue': '#00559B'
-			},
-			{
-				'saturation': 100
-			},
-			{
-				'lightness': -53
-			},
-			{
-				'visibility': 'simplified'
-			}
-			]
-		},
-		{
-			'featureType': 'water',
-			'elementType': 'all',
-			'stylers': [
-			{
-				'saturation': 100
-			},
-			{
-				'lightness': -60
-			},
-			{
-				'visibility': 'simplified'
-			},
-			{
-				'color': '#c9deff'
-			}
-			]
-		}
-		]
-	});
+		styles: map_styles
+	};
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	//contact_map = new google.maps.Map(document.getElementById('contact-map'), myOptions);
 
-	/*var image = '../images/map.svg';
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		//icon: image,
-		title: 'маркер 3'
-	});
-
-	var marker_2 = new google.maps.Marker({
-		position: {lat: 53.317664, lng: 50.417892},
-		map: map,
-		//icon: image,
-		title: 'маркер 2'
-	});
-
-	map.addListener('center_changed', function() {
-		window.setTimeout(function() {
-			map.panTo(marker.getPosition());
-		}, 3000);
-	});
-
-	marker.addListener('click', function() {
-		map.setZoom(9);
-		map.setCenter(marker.getPosition());
-	});*/
-
-	//var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	var markers = locations.map(function(location, i) {
 		return new google.maps.Marker({
 			position: location,
@@ -284,6 +262,33 @@ var locations = [
 {lat: 53.639176, lng: 49.493300}
 ];
 //Конец страницы Проекты - Карта
+
+//Страница Контакты - Карта
+function init_contact_Map() {
+	var myLatLng = {lat: 53.311315, lng: 50.528891};
+	contact_map = new google.maps.Map(document.getElementById('contact-map'), {
+		center: myLatLng,
+		zoom: 9,
+		scrollwheel: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		rotateControl: false,
+		fullscreenControl: false,
+		styles: map_styles
+	});
+
+	var image = '../images/marker-map.png';
+	//Маркер страница Контакты
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: contact_map,
+		icon: image
+	});
+
+};
+
+//Конец страницы Контакты - Карта
 
 $(document).ready(function() {
 	
